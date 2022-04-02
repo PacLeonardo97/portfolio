b74fd2d175fdd294f504aa6b90451f6acdec204c
Leonardo Pacheco <pacleonardo97@gmail.com>
2022-04-02 19:25:18 -0300

    change usestate form to formik

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,80 @@
-import { useState } from 'react';
-import { TextField, Button } from "@material-ui/core";
-import { Header, Slick } from '../components';
-import axios from 'axios';
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import { Header, Slick } from "../components";
+import { useFormik } from "formik";
+import axios from "axios";
 
 const Home = () => {
-  const [name, setName]= useState('');
-  const [email, setEmail]= useState('');
-  const [subject, setSubject]= useState('');
-  const [telephone, setTelephone]= useState('');
-  const [body, setBody]= useState('');
-  
-  const onSubmit = async e => {
-    e.preventDefault();
-
-    if(name && email && subject && telephone && body) {
-      try {
-       await axios.post('/api/contact', { name, email, subject, telephone, body})
-        
-      } catch (error) {
-        console.log('error =>>', error)
-      }
-    } else {
-      // alert('nada')
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      subject: "",
+      telephone: "",
+      body: "",
+    },
+    onSubmit: async (e) => {
+      console.log("e", e);
+      // await axios.post("/api/contact", {
+      //   name: e.name,
+      //   email: e.email,
+      //   subject: e.subject,
+      //   telephone: e.telephone,
+      //   body: e.body,
+      // });
+    },
+  });
 
   return (
     <>
       <Header />
       <Slick />
 
-      <form onSubmit={onSubmit} style={{ width: "650px", margin: '40px auto', textAlign: 'center' }}>
+      <form
+        onSubmit={formik.handleSubmit}
+        style={{ width: "650px", margin: "40px auto", textAlign: "center" }}
+      >
         <div style={{ marginBottom: "24px", display: "flex" }}>
           <div style={{ width: "50%", marginRight: "32px" }}>
-            <TextField onChange={e => setName(e.target.value)} fullWidth id="name" label="name" variant="outlined" />
+            <TextField
+              onChange={formik.handleChange}
+              fullWidth
+              id="name"
+              name="name"
+              label="name"
+              variant="outlined"
+            />
           </div>
           <div style={{ width: "50%" }}>
-            <TextField onChange={e => setEmail(e.target.value)} fullWidth id="email" label="email" variant="outlined" />
+            <TextField
+              onChange={formik.handleChange}
+              fullWidth
+              name="email"
+              id="email"
+              label="email"
+              variant="outlined"
+            />
           </div>
         </div>
         <div style={{ marginBottom: "24px", display: "flex" }}>
           <div style={{ width: "50%", marginRight: "32px" }}>
             <TextField
               fullWidth
-              id="Subject"
+              id="subject"
+              name="subject"
               label="Subject"
               variant="outlined"
-              onChange={e => setSubject(e.target.value)}
+              onChange={formik.handleChange}
             />
           </div>
           <div style={{ width: "50%" }}>
             <TextField
               fullWidth
               id="telephone"
+              name="telephone"
               label="telephone"
               variant="outlined"
-              onChange={e => setTelephone(e.target.value)}
+              onChange={formik.handleChange}
             />
           </div>
         </div>
@@ -65,13 +84,22 @@ const Home = () => {
             multiline
             rows={4}
             id="body"
+            name="body"
             label="body"
             variant="outlined"
-            onChange={e => setBody(e.target.value)}
+            onChange={formik.handleChange}
           />
         </div>
-        <div style={{ width:' 50%', margin: '0 auto' }}>
-          <Button fullWidth type="submit" variant="contained" color="primary" style={{marginTop:'32px'}}>Enviar</Button>
+        <div style={{ width: " 50%", margin: "0 auto" }}>
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{ marginTop: "32px" }}
+          >
+            Enviar
+          </Button>
         </div>
       </form>
     </>
